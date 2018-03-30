38fd146a7ba206cb77fa54948111c461501a03c7
sergiyarestenko <arestenkos@gmail.com>
2018-03-30 09:00:49 +0300

    lazy load img

--- src/js/_img_holders.js
+++ src/js/_img_holders.js
@@ -1,30 +1,3 @@
-this.setBackground = function () {
-    var imgHolders = $(".gt-img-holder");
-    imgHolders.each(function () {
-        if (!$(this).attr("data-bg")) return;
-        var path = $(this).attr("data-bg");
-        if (token667.is(":visible")) {
-            $(this).css(
-                "background-image",
-                'url("./img/backgrounds/phone/' + path + '")'
-            );
-            return;
-        }
-        if (token1024.is(":visible")) {
-            $(this).css(
-                "background-image",
-                'url("./img/backgrounds/tablet/' + path + '")'
-            );
-            return;
-        }
-        $(this).css(
-            "background-image",
-            'url("./img/backgrounds/desktop/' + path + '")'
-        );
-    });
-};
-
-
 this.imgHolderFunc = function (el) {
     $(el).removeClass("gt-width");
     $(el).removeClass("gt-height");
@@ -39,10 +12,66 @@ this.imgHolderFunc = function (el) {
     else {
         $(el).addClass("gt-height");
     }
-    $(el).addClass("gt-visible");
+    img.lazy({
+        placeholder: "data:image/gif;base64,R0lGODlhEALAPQAPzl5uLr9Nrl8e7...",
+        scrollDirection: 'vertical',
+        effect: 'fadeIn',
+        visibleOnly: true,
+        onError: function (element) {
+            console.log('error loading ' + element.data('src'));
+        }
+    });
 };
 
 
-if ($("div").is(".gt-img-holder")) {
-    self.setBackground();
+this.setBg = function (el) {
+    var bg,
+        currToken,
+        newToken,
+        resizeTimer;
+    setBg();
+
+    function setBg() {
+        if (token667.is(":visible")) {
+            bg = el.attr("data-src-phone");
+            currToken = 0;
+        } else if (token1024.is(":visible")) {
+            bg = el.attr("data-src-tablet");
+            currToken = 1;
+        } else {
+            bg = el.attr("data-src-desctop");
+            currToken = 2;
+        }
+        el.css("background-image", "url('./img/bgs/" + bg + "'");
+    }
+
+    $(window).resize(function () {
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(function () {
+
+            if (token667.is(":visible")) {
+                newToken = 0;
+            } else if (token1024.is(":visible")) {
+                newToken = 1;
+            } else {
+                newToken = 2;
+            }
+            if (newToken > currToken) {
+                setBg();
+            }
+        }, 250);
+    });
+};
+
+
+// todo resize TimeOut;
+
+
+if ($('div').is('.gt-bgs-holder')) {
+
+    $('.gt-bgs-holder').each(function () {
+        self.setBg($(this));
+    })
+
 }
+self.setBg($('#gt-footer'));
